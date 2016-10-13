4114824ec455e03a4a94be51e1e8297870c45c11
thomasdigby <thomasdigby1@gmail.com>
2016-10-13 14:18:46 +0100

    init

---
+++ agreable-standfirst-plugin.php
@@ -0,0 +1,20 @@
+
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: agreable-standfirst-plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-standfirst-plugin
+* Description: Introduces a standfirst field to the post Base Fields
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+class AgreableStandfirstPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableStandfirstPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Agreable Standfirst Plugin
+Introduces a standfirst field to the post Base Fields
